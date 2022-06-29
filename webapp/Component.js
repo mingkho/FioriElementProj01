8e511c151ad90af96cc36c077e7b79a20472ad1d
Ming Kho <ming.kho@sap.com>
2022-06-29 09:25:28 +0800

    initial

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("test.ns.lrop2806jlong.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
